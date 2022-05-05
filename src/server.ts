bba57b700e06f7a0dae8200d76347080c3dc998f
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-05 09:09:37 -0300

    :wrench: chore: setup server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import { routes } from './routes';
+
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.send('Faaaaaaaala dev');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server running on port 3333...');
